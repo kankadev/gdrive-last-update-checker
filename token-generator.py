ff8b68ef508e7ffe99a095ce4e303791b34f99fe
Akay Berk | kanka.dev <mail@kanka.dev>
2024-05-03 09:27:02 +0300

    Create token-generator.py

---
+++ token-generator.py
@@ -0,0 +1,25 @@
+import os
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# Pfade zu den Dateien
+credentials_path = 'credentials.json'
+token_path = 'token.json'
+
+# Berechtigungsbereiche
+scopes = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+
+def authorize():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        credentials_path,
+        scopes=scopes
+    )
+    creds = flow.run_local_server(port=0)
+    with open(token_path, 'w') as token_file:
+        token_file.write(creds.to_json())
+    print("Authorization successful. Token saved to 'token.json'.")
+
+
+if __name__ == '__main__':
+    authorize()
